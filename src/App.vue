5d4618d6e0b8da157f6d45dcbae1d46499becf43
Eddie Fabre <EddieNFab@Gmail.com>
2019-06-02 10:50:24 -0400

    updates

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
+  background-color:#DCDDDF;
+
   /*margin-top: 60px;*/
 }
 </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -11,37 +11,39 @@
       <h1>Calculator</h1>
 
       <div class="row">
-        <div class="col-12 display-box"></div>
+        <div class="col-12 display-box text-right">
+          <h2 class="">{{current || '0' }}</h2>
+        </div>
       </div>
 
       <div class="row first-row">
-        <button type="button" class="col-3">AC</button>
-        <button type="button" class="col-3">+/-</button>
-        <button type="button" class="col-3">%</button>
-        <button type="button" class="col-3">&#247;</button>
+        <button @click="clear" type="button" class="col-3">AC</button>
+        <button @click="sign" type="button" class="col-3">+/-</button>
+        <button @click="percent" type="button" class="col-3">%</button>
+        <button @click="divide" type="button" class="col-3">&#247;</button>
       </div>
       <div class="row second-row">
-        <button type="button" class="col-3">7</button>
-        <button type="button" class="col-3">8</button>
-        <button type="button" class="col-3">9</button>
-        <button type="button" class="col-3">x</button>
+        <button @click="append('7')" type="button" class="col-3">7</button>
+        <button @click="append('8')" type="button" class="col-3">8</button>
+        <button @click="append('9')" type="button" class="col-3">9</button>
+        <button @click="times" type="button" class="col-3">x</button>
       </div>
       <div class="row third-row">
-        <button type="button" class="col-3">4</button>
-        <button type="button" class="col-3">5</button>
-        <button type="button" class="col-3">6</button>
-        <button type="button" class="col-3">-</button>
+        <button @click="append('4')" type="button" class="col-3">4</button>
+        <button @click="append('5')" type="button" class="col-3">5</button>
+        <button @click="append('6')" type="button" class="col-3">6</button>
+        <button @click="minus" type="button" class="col-3">-</button>
       </div>
       <div class="row fourth-row">
-        <button type="button" class="col-3">1</button>
-        <button type="button" class="col-3">2</button>
-        <button type="button" class="col-3">3</button>
-        <button type="button" class="col-3">+</button>
+        <button @click="append('1')" type="button" class="col-3">1</button>
+        <button @click="append('2')" type="button" class="col-3">2</button>
+        <button @click="append('3')" type="button" class="col-3">3</button>
+        <button @click="add" type="button" class="col-3">+</button>
       </div>
       <div class="row sixth-row">
-        <button type="button" class="col-6">0</button>
-        <button type="button" class="col-3">.</button>
-        <button type="button" class="col-3">=</button>
+        <button @click="append('0')" type="button" class="col-6">0</button>
+        <button @click="dot" type="button" class="col-3">.</button>
+        <button @click="equal" type="button" class="col-3">=</button>
       </div>
 
     </div>
@@ -54,28 +56,95 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      previous:null,
+      current: '',
+      operator:null,
+      operatorClicked:false,
+    }
+  },
+  methods:{
+    clear(){
+      this.current = '';
+    },
+    sign(){
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+    },
+    percent(){
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number){
+      if(this.operatorClicked){
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot(){
+      if (this.current.indexOf('.') === -1){
+        this.append('.');
+      }
+    },
+    setPrevious(){
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide(){
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times(){
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus(){
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add(){
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal(){
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .display-box {
   background-color: #333;
-  min-height: 100px;
+  padding: 20px 10px;
 }
 
 .h100 {
   height: 100%;
 }
+h1 {
+  font-family: 'Cabin Sketch', cursive;
+  color: #333;
+}
+h2{
+  color: #fff;
+  margin-bottom: 0;
+}
 
 button {
   background-color: #cecfd2;
-  color:#fff;
+  color: #fff;
   padding: 15px 10px;
   border-style: solid;
   border-color: #333;
   border-width: thin;
+  font-family: 'Montserrat', sans-serif;
 }
 
 .second-row button {
